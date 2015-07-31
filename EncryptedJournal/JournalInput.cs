c30f2e52b61653ca85a5447b322a322f712d154f
BrandonRunyan <brando15@ksu.edu>
2015-07-30 22:46:37 -0500

    Changed name

--- EncryptedJournal/JournalInput.cs
+++ EncryptedJournal/JournalInput.cs
@@ -6,7 +6,7 @@ using System.Windows.Forms;
 
 namespace EncryptedJournal
 {
-    static class Program
+    static class JournalInput
     {
         /// <summary>
         /// The main entry point for the application.
